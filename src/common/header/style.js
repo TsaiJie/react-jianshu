b03f6e6d2b507cf2b43dbac8b65621d64d47f13a
tsai <mengjietsai@163.com>
2020-05-03 15:54:22 +0800

    动画代码优化

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -47,18 +47,7 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
   position: relative;
   float: left;
-  .slide-enter {
-    transition: all 0.2s ease-out;
-  }
-  .slide-enter-active {
-    width: 240px;
-  }
-  .slide-exit {
-    transition: all 0.2s ease-out;
-  }
-  .slide-exit-active {
-    width: 160px;
-  }
+
   .iconfont {
     position: absolute;
     right: 5px;
@@ -96,6 +85,18 @@ export const NavSearch = styled.input.attrs({
   }
   &.focused {
     width: 240px;
+  }
+  &.slide-enter {
+    transition: all 0.2s ease-out;
+  }
+  &.slide-enter-active {
+    width: 240px;
+  }
+  &.slide-exit {
+    transition: all 0.2s ease-out;
+  }
+  &.slide-exit-active {
+    width: 160px;
   }
 `
 export const Addition = styled.div`
