1edfff3ff4f33e5de1dee0d969f9dc2429505a2d
tsai <mengjietsai@163.com>
2020-05-04 08:31:24 +0800

    redux-immutable 的使用

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -52,7 +52,8 @@ const Header = props => {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    focused: state.header.get('focused')
+    focused: state.getIn(['header', 'focused'])
+    // focused: state.get('header').get('focused')
   }
 }
 const mapDispatchToProps = (dispatch, ownProps) => {
