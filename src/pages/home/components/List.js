58dbcbe5803c1936ee822afa82fac7c3b200d207
tsai <mengjietsai@163.com>
2020-05-05 10:53:14 +0800

    列表页完成

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import { ListItem, ListInfo } from '../style'
+import { connect } from 'react-redux'
 class List extends React.Component {
   render() {
-    return <div>List</div>
+    const { list } = this.props
+    return (
+      <React.Fragment>
+        {list.map(item => {
+          return (
+            <ListItem key={item.get('id')}>
+              <img className="pic" src={item.get('imgUrl')} alt=""></img>
+              <ListInfo>
+                <h3 className="title">{item.get('title')}</h3>
+                <p className="desc">{item.get('desc')}</p>
+              </ListInfo>
+            </ListItem>
+          )
+        })}
+      </React.Fragment>
+    )
   }
 }
-
-export default List
+const mapStateToProps = (state, ownProps) => {
+  return {
+    list: state.getIn(['home', 'articleList'])
+  }
+}
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(List)
